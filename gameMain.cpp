8c71b244037f9b004500a6e6160b2136ae3f35fd
Voldezhur <voldezhur@gmail.com>
2023-03-13 15:35:30 +0300

    Fixed main - now working

--- gameMain.cpp
+++ gameMain.cpp
@@ -2,7 +2,7 @@
 /*
     1. Make stats and actions scalable to allow for addition of new ones later
     2. Display both the player actions and actions appropriate for the location
-
+    3. Separate classes and main into different files
 */
 
 
@@ -14,7 +14,21 @@ struct stat
 {
     int value;
     std::string statName;
+
+    void operator++(int)
+    {
+        value++;
+    }
+
+    friend std::ostream &operator<<(std::ostream &stream, stat obj);
 };
+// overloading for stat
+std::ostream &operator<<(std::ostream &stream, stat obj)
+{
+    stream << obj.statName << (obj.statName.length() < 8 ? "\t " : " ") << "\t-\t" << obj.value;
+    return stream;
+}
+
 
 // character class
 class Character
@@ -24,22 +38,6 @@ public:
     stat str, end, lck;
     std::string name;
 
-    int stats[3] = {str.value, end.value, lck.value};
-
-    
-    // prints stats
-    void printStats()
-    {
-        std::cout << name << "'s stats:";
-        std::cout << "\nLevel\t-\t" << lvl;
-
-        
-        for(int i = 0; i <= 3; i++)
-        {
-            std::cout << '\n';
-        }
-    }
-
     // constructor
     Character(int lv, int s, int e, int lc)
     {
@@ -47,13 +45,26 @@ public:
         str.value = s;
         end.value = e;
         lck.value = lc;
+
+        str.statName = "Strength";
+        end.statName = "Endurance";
+        lck.statName = "Luck";
+    }
+
+
+    // prints stats
+    void printStats()
+    {
+        std::cout << name << "'s stats:";
+        std::cout << "\nLevel\t\t-\t" << lvl;
+        
+        std::cout << '\n' << str << '\n' << end << '\n' << lck << '\n';
     }
 };
 
 // location class
 class Location
 {
-public:
     // pointers to neighbouring locations
     Location* south;
     Location* west;
@@ -64,6 +75,109 @@ public:
     std::string flavourText, description;
 
     bool canForage, canHunt, isHostile;
+
+public:
+    // default constructor
+    Location()
+    {
+        south = west = north = east = nullptr;
+
+        flavourText = "placeholderFlavourText";
+        description = "placeholderDescription";
+
+        canForage = canHunt = isHostile = true;
+    }
+
+    // constructor
+    Location(bool _canForage, bool _canHunt, bool _isHostile)
+    {
+        Location();
+
+        canForage = _canForage;
+        canHunt = _canHunt;
+        isHostile = _isHostile;
+    }
+
+
+    // ==========================
+    // set methods
+    
+    void setSouth(Location* _south)
+    {
+        south = _south;
+        _south->north = this;
+    }
+
+    void setWest(Location* _west)
+    {
+        west = _west;
+        _west->east = this;
+    }
+
+    void setNorth(Location* _north)
+    {
+        north = _north;
+        _north->south = this;
+    }
+
+    void setEast(Location* _east)
+    {
+        east = _east;
+        _east->west = this;
+    }
+
+    void setFlavourText(std::string S)
+    {
+        flavourText = S;
+    }
+    
+    void setDescription(std::string S)
+    {
+        description = S;
+    }
+    
+
+    // ==========================
+    // get methods
+
+    std::string getDescription()
+    {
+        return description;
+    }    
+
+    Location* getSouth()
+    {
+        return south;
+    }
+
+    Location* getWest()
+    {
+        return west;
+    }
+
+    Location* getNorth()
+    {
+        return north;
+    }
+
+    Location* getEast()
+    {
+        return east;
+    }
+
+
+    // ==========================
+    // general gameplay methods
+
+    void lookAround()
+    {
+        std::cout << "You find yourself in " << flavourText << "\n\n";
+    }
+
+    void printFlavourText()
+    {
+        std::cout << flavourText << '\n';
+    }
 };
 
 // player actions
@@ -84,7 +198,7 @@ int main()
     
     while(skillPoints)
     {
-        std::cout << "\nStrength\t-\t" << player.str << "\nEndurance\t-\t" << player.end << "\nLuck\t\t-\t" << player.lck << "\n\n";
+        std::cout << '\n' << player.str << '\n' << player.end << '\n' << player.lck << "\n\n";
         std::cout << "You have " << skillPoints << " points to spend\n";
         std::cout << "\nMake a selection:\n";
 
@@ -131,7 +245,7 @@ int main()
 
     // final stat output
     std::cout << "\nFinal stats:";
-    std::cout << "\nStrength\t-\t" << player.str << "\nEndurance\t-\t" << player.end << "\nLuck\t\t-\t" << player.lck << "\n\n";
+    std::cout << '\n' << player.str << '\n' << player.end << '\n' << player.lck << "\n\n";
 
     // skill confirmation
     std::cout << "\nAre these stats ok?\n";
@@ -141,10 +255,10 @@ int main()
 
     std::string input;
     
-    // check if input is one character long
     statConfirmLoop:
     std::cin >> input;
 
+    // check if input is one character long
     while(input.length() != 1)
     {
         std::cout << "\nIncorrect choice\n";
@@ -184,14 +298,13 @@ int main()
 
     
     // map creation
-    Location spawnArea;
-    spawnArea.isHostile = false;
-    spawnArea.flavourText = "a clearing of a forest, filled with all types of flowers and wild plants.\nYou sometimes see an occasional squirrel jump from one tree to another";
+    Location* spawnArea = new Location(true, true, false);
+    spawnArea->setFlavourText("a clearing of a forest, filled with all types of flowers and wild plants.\nYou sometimes see an occasional squirrel jump from one tree to another");
 
 
 
     // game start
-    Location playerLocation = spawnArea;
+    Location* playerLocation = spawnArea;
 
     std::cout << "You awaken from your nap in the forest clearing, gather your supplies and prepare to move out\n";    
     std::cout << "What do you do?\n";
@@ -215,7 +328,7 @@ int main()
             // look around
             case 1:
             {
-                playerLocation.printFlavourText();
+                playerLocation->printFlavourText();
             }
             break;
             
@@ -243,4 +356,6 @@ int main()
 
     // game end
     std::cout << "\nGame ended\n";
+
+    delete spawnArea;
 }
