412035d7837a0106569dc6c6c0fa1aadf020096b
Voldezhur <voldezhur@gmail.com>
2023-04-14 19:34:56 +0300

    added item class . no functionality yet, just attributes

---
+++ classes/Item.cpp
@@ -0,0 +1,9 @@
+#include "Item.h"
+
+
+Item::Item(std::string _name)
+{
+    name = _name;
+    description = "*placeholderDescription*";
+    price = 0;
+}
---
+++ classes/Item.h
@@ -0,0 +1,11 @@
+#include <string>
+
+class Item
+{    
+public:
+    std::string name, description;
+
+    int price;
+
+    Item(std::string _name);
+};
